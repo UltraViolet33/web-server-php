cd751aaf43b4b52ea60e1aa973df7c8fd5034f98
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-19 11:42:33 +0100

    listen method for server class

--- src/Server.php
+++ src/Server.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use Request;
+use Response;
+use Exception;
 use Socket;
 
 class Server
@@ -36,6 +39,42 @@ class Server
     {
         if (!socket_bind($this->socket, $this->host, $this->port)) {
             throw new \Exception('Could not bind: ' . $this->host . ':' . $this->port . ' - ' . socket_strerror(socket_last_error()));
+        }
+    }
+
+    public function listen(callable $callback): void
+    {
+        // check if the callback is valid
+        if (!is_callable($callback)) {
+            throw new Exception("The argument should be callable");
+        }
+
+
+        while (true) {
+            //listen for connections
+            socket_listen($this->socket);
+
+
+            if (!$client = socket_accept($this->socket)) {
+                socket_close($client);
+                continue;
+            }
+
+
+            $request = Request::withHeaderString(socket_read($client, 1024));
+
+            $response = call_user_func($callback, $request);
+
+
+            if (!$response || !$response instanceof Response) {
+                $response = Response::error(404);
+            }
+
+            $response = (string) $response;
+
+            socket_write($client, $response, strlen($response));
+
+            socket_close($client);
         }
     }
 }
